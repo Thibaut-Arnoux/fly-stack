ec6e74947c6a6fe24b7bbdfe30528b66e272d0fd
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-03-31 12:24:20 +0200

    feat: add react-query client and devtools

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,14 @@
+import { queryClient } from '@/query-client';
 import { router } from '@/router';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
 
 export const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 };
