cce1acaf3dd28c2f82f0b340762acf9d956d52a4
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-04-11 09:57:19 +0200

    feat: init table, takes all height with scrollbar and pagination at the end

--- src/routes/items.tsx
+++ src/routes/items.tsx
@@ -20,24 +20,51 @@ function Items() {
   const { data: items } = useItemsData({ page: page });
 
   return (
-    <div className="p-2">
-      <h3>Items :</h3>
-      {items.data.map((item) => (
-        <div key={item.id}>
-          <h4>{item.name.en}</h4>
-        </div>
-      ))}
+    <>
+      <div className="flex-1 overflow-y-auto p-2">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Icon</th>
+              <th>Name</th>
+              <th>Sex</th>
+              <th>Level</th>
+              <th>Rarity</th>
+              <th>Category</th>
+              <th>SubCategory</th>
+              <th>Sell</th>
+            </tr>
+          </thead>
 
-      <Pagination
-        page={page}
-        onPageChange={setPage}
-        pageStatus={{
-          first: items.first,
-          prev: items.prev,
-          next: items.next,
-          last: items.last,
-        }}
-      />
-    </div>
+          <tbody>
+            {items.data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.icon}</td>
+                <td>{item.name.en}</td>
+                <td>{item.sex}</td>
+                <td>{item.level}</td>
+                <td>{item.rarity}</td>
+                <td>{item.category}</td>
+                <td>{item.subcategory}</td>
+                <td>{item.sellPrice}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="flex justify-center my-1">
+        <Pagination
+          page={page}
+          onPageChange={setPage}
+          pageStatus={{
+            first: items.first,
+            prev: items.prev,
+            next: items.next,
+            last: items.last,
+          }}
+        />
+      </div>
+    </>
   );
 }
