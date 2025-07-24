d35400c2a96c85d355116049a23c67e0b0bfaeec
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-07-24 16:09:54 +0200

    wip

--- src/components/features/items/filter-item.tsx
+++ src/components/features/items/filter-item.tsx
@@ -1,3 +1,18 @@
 export const FilterItem = () => {
-  return <h1>Side Filter</h1>;
+  return (
+    <ul className="menu rounded-box w-full">
+      <li>
+        <a>Parent</a>
+        <ul>
+          <li>
+            <a>
+              <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+                Childssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+              </span>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  );
 };
