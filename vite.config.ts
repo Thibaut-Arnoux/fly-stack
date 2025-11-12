f35d2869a88ebc3edca035d24738e61aef51a904
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-11-12 14:30:28 +0100

    refactor: up-to-date vite and tsconfig with new template from tanstack

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    react(),
+  ],
+});
