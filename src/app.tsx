1214f3b0f450c66169741b7845a6adac58b01673
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-11-12 10:25:26 +0100

    refactor: remove @tanstack/react-query-devtools

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,4 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
 import { queryClient } from '@/query-client';
 import { router } from '@/router';
@@ -8,7 +7,6 @@ export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 };
